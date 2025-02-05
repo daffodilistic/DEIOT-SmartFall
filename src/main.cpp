392e429fcf08fe5e0596b104a54479483ad9d2e4
Soh Thiam Hing <daffodilistic@gmail.com>
2025-02-05 15:16:48 +0800

    Initial working code with M5StickC

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <PubSubClient.h>
-#include <Adafruit_MPU6050.h>
+#include "M5Unified.h"
+// #include <Adafruit_MPU6050.h>
 
 #define LED_PIN_RED 10
+#define ANALOG_PIN 34
 #define FALL_LED_FLASH_INTERVAL_MS 500
 #define FALL_ALARM_DURATION_MS 10000
-#define FALL_DETECTION_MS 1000
-#define FALL_ACCELERATION_G 14.715
+#define FALL_DETECTION_MS 500
+#define FALL_ACCELERATION_G 2
 #define MQTT_ENDPOINT "broker.emqx.io"
 #define MQTT_PORTNUM 1883
 
@@ -17,40 +19,50 @@ bool ledRedOn = false;
 bool hasFallen = false;
 char* WIFI_SSID = "Wokwi-GUEST";
 char* WIFI_PASSWORD = "";
-char* MQTT_DEFAULT_TOPIC = "DEIOT/SmartFall";
+char MQTT_DEFAULT_TOPIC[] = "DEIOT/SmartFall";
 char MQTT_CLIENT_NAME[32] = "SmartFall_";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-Adafruit_MPU6050 mpu;
-sensors_event_t acc, gcc, temp;
+// Adafruit_MPU6050 mpu;
+// sensors_event_t acc, gcc, temp;
 
-void led_flash(unsigned long currentMs) {
+void led_flash(unsigned long currentMs)
+{
   unsigned long duration = currentMs - ledPreviousMs;
   // Serial.println(duration);
-  if (duration >= FALL_LED_FLASH_INTERVAL_MS) {
+  if (duration >= FALL_LED_FLASH_INTERVAL_MS)
+  {
     ledRedOn = !ledRedOn;
     ledPreviousMs = currentMs;
   }
 
-  if (ledRedOn) {
-    digitalWrite(LED_PIN_RED, HIGH);
-  } else {
+  if (ledRedOn)
+  {
     digitalWrite(LED_PIN_RED, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_PIN_RED, HIGH);
   }
 }
 
-void mqtt_generateClientName() {
+void mqtt_generateClientName()
+{
   char *buffer = &MQTT_CLIENT_NAME[10];
-  randomSeed(analogRead(0));
-  for (int i = 0; i < 12; i++) {
+  randomSeed(analogRead(ANALOG_PIN));
+  for (int i = 0; i < 12; i++)
+  {
     bool isNumber = random(0, 2);
-    if (isNumber) {
+    if (isNumber)
+    {
       // Use ASCII table trickery because of laziness
-      buffer[i] = (char) random(48, 58);
-    } else {
+      buffer[i] = (char)random(48, 58);
+    }
+    else
+    {
       // Use ASCII table trickery because of laziness
-      buffer[i] = (char) random(65, 91);
+      buffer[i] = (char)random(65, 91);
     }
   }
   //(MQTT_CLIENT_NAME+1) = buffer[0];
@@ -60,10 +72,10 @@ void mqtt_generateClientName() {
 
 void mqtt_connect()
 {
-  while (!client.connected() )
+  while (!client.connected())
   {
     Serial.println("Connecting to MQTT ...");
-    if (client.connect(MQTT_CLIENT_NAME) ) //, mqttUser, mqttPassword) )
+    if (client.connect(MQTT_CLIENT_NAME)) //, mqttUser, mqttPassword) )
     {
       Serial.println("Connected");
       client.subscribe(MQTT_DEFAULT_TOPIC);
@@ -71,29 +83,137 @@ void mqtt_connect()
     else
     {
       Serial.print("Failed with state ");
-      Serial.println(client.state() );
+      Serial.println(client.state());
       delay(2000);
     }
   }
 }
 
-void mqtt_publish(char* topic, char* payload)
+void mqtt_publish(char *topic, char *payload)
 {
   char buffer[256];
   sprintf(buffer, "%s", payload);
   client.publish(topic, buffer);
 }
 
-void mqtt_setup() {
+void mqtt_setup()
+{
   mqtt_generateClientName();
   client.setServer(MQTT_ENDPOINT, MQTT_PORTNUM);
+}
+
+float mpu_get_acceleration_z()
+{
+  float z_accel = 0.0f;
+  // TODO should calibrate and use big data/ML to detect a fall!
+  // mpu.getEvent(&acc, &gcc, &temp);
+
+  // Serial.print("Z-axis acceleration: ");
+  // Serial.println(acc.acceleration.z);
+
+  auto imu_update = M5.Imu.update();
+  if (imu_update)
+  {
+    auto data = M5.Imu.getImuData();
+    z_accel = data.accel.z;
+    // char buffer[64];
+    // sprintf(buffer, "ax:%f  ay:%f  az:%f", data.accel.x, data.accel.y, data.accel.z);
+    // Serial.println(buffer);
+  }
+  else
+  {
+    M5.update();
+  }
+
+  return z_accel;
+}
+
+void mpu_setup()
+{
+  auto cfg = M5.config();
+  cfg.serial_baudrate = 9600;
+  M5.begin(cfg);
+
+  const char *name;
+  auto imu_type = M5.Imu.getType();
+  // switch (imu_type)
+  // {
+  // case m5::imu_none:
+  //   name = "not found";
+  //   break;
+  // case m5::imu_sh200q:
+  //   name = "sh200q";
+  //   break;
+  // case m5::imu_mpu6050:
+  //   name = "mpu6050";
+  //   break;
+  // case m5::imu_mpu6886:
+  //   name = "mpu6886";
+  //   break;
+  // case m5::imu_mpu9250:
+  //   name = "mpu9250";
+  //   break;
+  // case m5::imu_bmi270:
+  //   name = "bmi270";
+  //   break;
+  // default:
+  //   name = "unknown";
+  //   break;
+  // };
+  // M5_LOGI("imu:%s", name);
+
+  if (imu_type == m5::imu_none)
+  {
+    for (;;)
+    {
+      delay(1);
+    }
+  }
+
+  // if (!mpu.begin()) {
+  //   while (1) {
+  //     delay(20);
+  //   }
+  // }
+}
+
+void display_ok()
+{
+  lgfx::v1::TextStyle ts = lgfx::v1::TextStyle();
+  ts.back_rgb888 = 0x00FF00;
+  ts.fore_rgb888 = 0x000000;
+  // 0 = 270 deg / 1 = normal / 2 = 90 deg / 3 = 180 deg / 4~7 = upside down
+  M5.Display.setRotation(3);
+  M5.Display.setColor(0, 255, 0);
+  M5.Display.fillRect(0, 0, 160, 80);
+  M5.Display.setCursor(80 - (26 / 2), 40 - (26 / 2));
+  // From https://m5stack.lang-ship.com/howto/m5gfx/font/
+  M5.Display.setFont(&fonts::Font4);
+  M5.Display.setTextStyle(ts);
+  M5.Display.print("OK");
+}
+
+void display_fall()
+{
+  lgfx::v1::TextStyle ts = lgfx::v1::TextStyle();
+  ts.back_rgb888 = 0xFF0000;
+  ts.fore_rgb888 = 0xFFFFFF;
+  // 0 = 270 deg / 1 = normal / 2 = 90 deg / 3 = 180 deg / 4~7 = upside down
+  M5.Display.setRotation(3);
+  M5.Display.setColor(255, 0, 0);
+  M5.Display.fillRect(0, 0, 160, 80);
+  M5.Display.setCursor(80 - (26 / 2 * 2), 40 - (26 / 2));
+  // From https://m5stack.lang-ship.com/howto/m5gfx/font/
+  M5.Display.setFont(&fonts::Font4);
+  M5.Display.setTextStyle(ts);
+  M5.Display.print("FALL");
 }
 
 void wifi_setup()
 {
   Serial.print("Connecting to Wi-Fi ...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED )
+  while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
     Serial.print(".");
@@ -105,20 +225,17 @@ void wifi_setup()
   Serial.println(WiFi.localIP());
 }
 
-void setup() {
+void setup()
+{
   unsigned long startTime = millis();
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Hello, ESP32!");
 
   pinMode(LED_PIN_RED, OUTPUT);
+  digitalWrite(LED_PIN_RED, HIGH);
 
-  if (!mpu.begin()) {
-    while (1) {
-      delay(20);
-    }
-  }
-
+  mpu_setup();
   wifi_setup();
   mqtt_setup();
 
@@ -126,43 +243,51 @@ void setup() {
   Serial.print("Finished setup in ");
   Serial.print(initTime);
   Serial.println("ms");
+
+  display_ok();
 }
 
-void loop() {
-  if ( !client.connected() )
+void loop()
+{
+  if (!client.connected())
   {
     mqtt_connect();
   }
   client.loop();
 
   unsigned long currentMs = millis();
-  if (hasFallen == false) {
-    if (currentMs - previousMs >= FALL_DETECTION_MS) {
-      // TODO should calibrate and use big data/ML to detect a fall!
-      mpu.getEvent(&acc, &gcc, &temp);
-
-      Serial.print("Z-axis acceleration: ");
-      Serial.println(acc.acceleration.z);
-      float z_acc = acc.acceleration.z;
-      if (z_acc >= FALL_ACCELERATION_G) {
+  if (hasFallen == false)
+  {
+    if (currentMs - previousMs >= FALL_DETECTION_MS)
+    {
+      float z_acc = mpu_get_acceleration_z();
+      if (z_acc >= FALL_ACCELERATION_G)
+      {
         char buffer[256];
         sprintf(buffer, "%s,EVENT_FALL,%f", MQTT_CLIENT_NAME, z_acc);
         Serial.println(buffer);
         mqtt_publish(MQTT_DEFAULT_TOPIC, buffer);
 
         hasFallen = true;
+        display_fall();
       }
 
       previousMs = currentMs;
       ledPreviousMs = currentMs;
     }
-  } else {
-    if (currentMs - previousMs <= FALL_ALARM_DURATION_MS) {
+  }
+  else
+  {
+    if (currentMs - previousMs <= FALL_ALARM_DURATION_MS)
+    {
       led_flash(currentMs);
-    } else {
-      digitalWrite(LED_PIN_RED, LOW);
+    }
+    else
+    {
+      digitalWrite(LED_PIN_RED, HIGH);
       previousMs = currentMs;
       hasFallen = false;
+      display_ok();
     }
   }
 
