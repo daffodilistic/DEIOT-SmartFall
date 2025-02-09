69518e446abca9bf5920a4a416e79332d1926854
Soh Thiam Hing <daffodilistic@gmail.com>
2025-02-09 16:46:59 +0800

    [feature] Add CORS config on firebase function

--- cloud/firebase/functions/index.js
+++ cloud/firebase/functions/index.js
@@ -3,9 +3,14 @@ const { logger } = require("firebase-functions");
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
+const webRequestConfig = {
+    cors: ["https://example.com"],
+    region: 'asia-southeast1'
+}
+
 initializeApp();
 
-exports.addmessage = onRequest({ region: 'asia-southeast1' }, async (req, res) => {
+exports.addmessage = onRequest(webRequestConfig, async (req, res) => {
     if (req.method !== "POST") {
         res.status(405).send("Method Not Allowed");
         return;
