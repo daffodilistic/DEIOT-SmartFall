9c88f98b63660ac40fa2cf2553388e3c75ccabe3
Soh Thiam Hing <daffodilistic@gmail.com>
2025-02-13 11:37:31 +0800

    [feature] Add Google Analytics

--- cloud/firebase/functions/index.js
+++ cloud/firebase/functions/index.js
@@ -3,11 +3,22 @@ const { logger } = require("firebase-functions");
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const webRequestConfig = {
+let config = null;
+if (process.env.FUNCTIONS_EMULATOR) {
+  config = {
+    cors: ["http://127.0.0.1:5000"],
+    region: "asia-southeast1",
+  };
+  console.log("Running in emulator mode");
+} else {
+  config = {
     cors: ["https://example.com"],
-    region: 'asia-southeast1'
+    region: "asia-southeast1",
+  };
+  console.log("Running in production mode");
 }
 
+const webRequestConfig = Object.freeze(config);
 initializeApp();
 
 exports.addmessage = onRequest(webRequestConfig, async (req, res) => {
@@ -19,5 +30,6 @@ exports.addmessage = onRequest(webRequestConfig, async (req, res) => {
   const deviceId = jsonBody.device;
   const newDocument = await getFirestore().collection("devices").doc(deviceId);
   newDocument.collection("logs").add(jsonBody);
+
   res.json({ result: `Message with ID: ${newDocument.id} added.` });
 });
